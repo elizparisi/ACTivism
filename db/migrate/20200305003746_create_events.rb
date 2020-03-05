11f9570a6d098ad2b90c44a8cb6847a7cb928046
Elizabeth Parisi <parisielizabeth@gmail.com>
2020-03-04 19:43:09 -0500

    ran migrations

--- db/migrate/20200305003746_create_events.rb
+++ db/migrate/20200305003746_create_events.rb
@@ -6,8 +6,7 @@ class CreateEvents < ActiveRecord::Migration[6.0]
       t.string :time
       t.text :location
       t.text :description
-      t.boolean, :attending
-      t.false :default
+      t.boolean :attending, default: false 
       t.belongs_to :user, null: false, foreign_key: true
       t.belongs_to :group, null: false, foreign_key: true
 
